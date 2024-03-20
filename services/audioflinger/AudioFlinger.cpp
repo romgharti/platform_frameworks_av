a1b9c15c1699ec62d4ae462269f6e97ad6e137c9
someone5678 <someone5678@users.noreply.github.com>
2024-03-20 11:10:40 +0900

    fixup! av: support per-app volume [1/3]
    Change-Id: I669bacfb8ee4a7a60c15a8dac4c487e2eccc94c6
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- services/audioflinger/AudioFlinger.cpp
+++ services/audioflinger/AudioFlinger.cpp
@@ -1121,7 +1121,7 @@ status_t AudioFlinger::createTrack(const media::CreateTrackRequest& _input,
         if (lStatus == NO_ERROR) {
             // set volume
             String8 trackCreatorPackage = track->getPackageName();
-            if (!trackCreatorPackage.isEmpty() &&
+            if (!trackCreatorPackage.empty() &&
                 mAppVolumeConfigs.find(trackCreatorPackage) != mAppVolumeConfigs.end()) {
                 media::AppVolume config = mAppVolumeConfigs[trackCreatorPackage];
                 track->setAppMute(config.muted);
@@ -2022,10 +2022,9 @@ uint32_t AudioFlinger::getInputFramesLost(audio_io_handle_t ioHandle) const
 status_t AudioFlinger::listAppVolumes(std::vector<media::AppVolume> *vols)
 {
     std::set<media::AppVolume> volSet;
-    Mutex::Autolock _l(mLock);
+    audio_utils::lock_guard _l(mutex());
     for (size_t i = 0; i < mPlaybackThreads.size(); i++) {
-        sp<PlaybackThread> thread = mPlaybackThreads.valueAt(i);
-        thread->listAppVolumes(volSet);
+        mPlaybackThreads.valueAt(i)->listAppVolumes(volSet);
     }
 
     vols->insert(vols->begin(), volSet.begin(), volSet.end());
@@ -2035,10 +2034,9 @@ status_t AudioFlinger::listAppVolumes(std::vector<media::AppVolume> *vols)
 
 status_t AudioFlinger::setAppVolume(const String8& packageName, const float value)
 {
-    Mutex::Autolock _l(mLock);
+    audio_utils::lock_guard _l(mutex());
     for (size_t i = 0; i < mPlaybackThreads.size(); i++) {
-        sp<PlaybackThread> t = mPlaybackThreads.valueAt(i);
-        t->setAppVolume(packageName, value);
+        mPlaybackThreads.valueAt(i)->setAppVolume(packageName, value);
     }
 
     if (mAppVolumeConfigs.find(packageName) == mAppVolumeConfigs.end()) {
@@ -2055,10 +2053,9 @@ status_t AudioFlinger::setAppVolume(const String8& packageName, const float valu
 
 status_t AudioFlinger::setAppMute(const String8& packageName, const bool value)
 {
-    Mutex::Autolock _l(mLock);
+    audio_utils::lock_guard _l(mutex());
     for (size_t i = 0; i < mPlaybackThreads.size(); i++) {
-        sp<PlaybackThread> t = mPlaybackThreads.valueAt(i);
-        t->setAppMute(packageName, value);
+        mPlaybackThreads.valueAt(i)->setAppMute(packageName, value);
     }
 
     if (mAppVolumeConfigs.find(packageName) == mAppVolumeConfigs.end()) {
